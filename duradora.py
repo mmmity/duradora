1c9d1f95a08bb86546bfb1bc395219232450159a
mmmity <baidurik.media.official@gmail.com>
2024-05-04 20:57:38 +0300

    Added track endpoints

--- duradora.py
+++ duradora.py
@@ -1,14 +1,17 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, UploadFile, Form, File
 from fastapi.responses import StreamingResponse
-from typing import Annotated
+from typing import Annotated, Optional
 
 from src.db.user_controller import User, UserController
-from src.db import userController
 from src.auth import Auth
+
+from src.db.track_controller import TrackController, Track
+from src.tracks import TrackHandler, TrackWithFile, DBTrackWithFile
 
 from pydantic import BaseModel
 
 auth = Auth()
+tracks = TrackHandler()
 app = FastAPI()
 
 class Error(BaseModel):
@@ -43,3 +46,19 @@ async def update_user(user: User):
     except Exception as e:
         return {'error': str(e)}
     return {'success': True}
+
+@app.post('/track/add')
+async def add_track(title: str | None = None, artists: str | None = None, file: Annotated[UploadFile, File()] = None):
+    return await tracks.add_track(TrackWithFile(title=title, artists=artists, file=file))
+
+@app.post('/track/update')
+async def update_track(uuid: str, title: str | None = None, artists: str | None = None, file: Annotated[UploadFile, File()] = None):
+    return await tracks.update_track(DBTrackWithFile(uuid=uuid, title=title, artists=artists, file=file))
+
+@app.get('/track')
+async def get_track(uuid: str):
+    return await tracks.get_track(uuid)
+
+@app.get('/stream')
+def stream_track(uuid: str):
+    return tracks.stream_track(uuid)
