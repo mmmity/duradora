c11a70de0e7ab9c9d3581eb387f93eb661e0781b
mmmity <baidurik.media.official@gmail.com>
2024-05-06 00:11:35 +0300

    Added hashing to admin password

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 from src import config
 from src.db.user_controller import UserController, User
+from src.hashes import SHA256Hasher
 
 controller = UserController(config.DB_PATH)
 if controller.find_user('admin') is None:
-    controller.create_user(User(username='admin', password=config.ADMIN_PASSWORD, is_admin=True))
+    controller.create_user(User(username='admin', password=SHA256Hasher(config.SALT).hexdigest(config.ADMIN_PASSWORD), is_admin=True))
